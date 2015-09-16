37c639d79667f868c8cbdf0c71eb31965466cb9c
unknown <rgraef@DOAVW7P2169.jsi.jc.com>
2015-09-16 14:23:08 -0500

    lighting support and opaque rectangles

--- app.js
+++ app.js
@@ -18,17 +18,36 @@ MainState.prototype = {
         //initialize the phaser-illuminated object and functions
         this._phaserIlluminated = new PhaserIlluminated(game);
 
+        myBackgroundBmd = game.add.bitmapData(game.width, game.height);
+        myBackgroundBmd.ctx.fillStyle = "#000000";
+        myBackgroundBmd.ctx.fillRect(0, 0, game.width, game.height);
+        game.cache.addBitmapData('background', myBackgroundBmd);
+        myBackgroundSprite = game.add.sprite(0, 0, myBackgroundBmd);
+
         //add a lamp to the game
-        game.add.illuminated.lamp(0, 0);
-        game.add.illuminated.darkMask();
+        myLamp1 = game.add.illuminated.lamp(0, 0);
+        //myMask = game.add.illuminated.darkMask();
+
+        myLamp2 = game.add.illuminated.lamp(0, 0);
+        //myMask.bringToTop();
+        //myMask.addLampSprite(myLamp2);
+
+        myObj = game.add.illuminated.rectangleObject(120, 120, 40, 30);
+        myObjs = [myObj];
+        myLamp2.createLighting(myObjs);
+        myLamp1.createLighting(myObjs);
     },
 
     update: function(){
-        
+        myLamp1.refresh();
+        myLamp2.refresh();
+        //myMask.refresh();
+        myLamp1.y -= 0.5;
+        myLamp2.y += 0.5;
     },
 
     render: function(){
-        
+
     }
 }
 
--- src/phaser-illuminated.js
+++ src/phaser-illuminated.js
@@ -7,23 +7,35 @@ PhaserIlluminated.prototype = {
     _game: null,
 
     _construct: function(){
+        //these guys function as sprites
         this._game.add.illuminated = {};
         this._game.add.illuminated.lamp = this._createLamp;
         this._game.add.illuminated.darkMask = this._createDarkMask;
-        this._game.add.illuminated.lighting = this._createLighting;
 
-        //this is a bit hacky but the add calls are from the scope of the illuminated object, so we create an array to keep
-        //track of cached bitmapdata objects here
+        //these guys function as their regular illuminated.js counterparts
+        this._game.add.illuminated.rectangleObject = this._createRectangleObject;
+
+        //this is a bit hacky but the add calls are from the scope of the illuminated object, so we put needed
+        //variables here
         this._game.add.illuminated._illuminatedSprites = [];
+        this._game.add.illuminated._opaqueObjects = [];
         this._game.add.illuminated._game = this._game;
+
+        //override this to respect the sprite offset
+        illuminated.Lamp.prototype.mask = function (ctx) {
+            var c = this._getVisibleMaskCache();
+            var orientationCenter = new illuminated.Vec2(Math.cos(this.angle), -Math.sin(this.angle)).mul(this.roughness*this.distance);
+            ctx.drawImage(c.canvas, this.offset.x + Math.round(this.position.x+orientationCenter.x-c.w/2), this.offset.y + Math.round(this.position.y+orientationCenter.y-c.h/2));
+        }
     },
 
     _createLamp: function(x, y, config){
-        var radius;
-        if(!config || !config.radius){
-            radius = 200;
+        //distance is the actual distance the light travels
+        var distance;
+        if(!config || !config.distance){
+            distance = 200;
         }else{
-            radius = config.radius;
+            distance = config.distance;
         }
 
         if(!x){
@@ -34,22 +46,59 @@ PhaserIlluminated.prototype = {
             y = 0;
         }
 
-        var bmd = game.add.bitmapData(radius*2, radius*2);
+        var bmd = game.add.bitmapData(distance*2, distance*2);
         game.cache.addBitmapData('illuminated-lamp-'+this._illuminatedSprites.length, bmd);
-        var lamp = new illuminated.Lamp({position: new illuminated.Vec2(radius, radius)});
-        lamp.render(bmd.ctx);
+        var lamp = new illuminated.Lamp({position: new illuminated.Vec2(distance, distance), distance: distance});
+        lamp.offset = {};
         var sprite = game.add.sprite(x, y, bmd);
         sprite.bmdIndex = 'illuminated-lamp-'+this._illuminatedSprites.length;
         sprite.bmd = bmd;
         sprite.lamp = lamp;
+
+        //update the bmd and update the position of the lamp for masking and lighting purposes
         sprite.refresh = function(){
-            this.lamp.render(this.bmd.ctx);
+            this.bmd.ctx.clearRect(0, 0, this.bmd.width, this.bmd.height);
+
+            if(this.lighting){
+                //render solid objects relative to position of sprites
+                this.lighting.objects.forEach(function(o){
+                    o.topleft.x = o.originalX - this.x;
+                    o.topleft.y = o.originalY - this.y;
+                    o.bottomright.x = o.originalX + o.originalW - this.x;
+                    o.bottomright.y = o.originalY + o.originalH - this.y
+                    o.syncFromTopleftBottomright();
+                }, this);
+
+                this.lighting.compute(this.bmd.width, this.bmd.height);
+                this.lighting.render(this.bmd.ctx);
+            }else{
+                this.lamp.render(this.bmd.ctx);
+            }
+
             this.bmd.dirty = true;
+            this.lamp.offset.x = this.x;
+            this.lamp.offset.y = this.y;
+        }
+        sprite.createLighting =  function(opaqueObjects){
+            if(!opaqueObjects){
+                return null;
+            }
+
+            var bmd = this.bmd;
+
+            var lighting = new illuminated.Lighting({light: lamp, objects: opaqueObjects});
+            lighting.compute(bmd.width, bmd.height);
+            lighting.render(bmd.ctx);
+
+            this.lighting = lighting;
+
+            return this;
         }
         sprite.getLamp = function(){
             return this.lamp;
         }
 
+        sprite.refresh();
 
         this._illuminatedSprites.push(sprite);
         return sprite;
@@ -68,7 +117,7 @@ PhaserIlluminated.prototype = {
 
         var bmd = game.add.bitmapData(this._game.width, this._game.height);
         game.cache.addBitmapData('illuminated-darkmask', bmd);
-        var darkMask = new illuminated.DarkMask({lights: lamps, color: color ? color : 'rgba(255,255,255,0.8'});
+        var darkMask = new illuminated.DarkMask({lights: lamps, color: color ? color : 'rgba(0,0,0,0.8'});
         darkMask.compute(this._game.width, this._game.height);
         darkMask.render(bmd.ctx);
         var sprite = game.add.sprite(0, 0, bmd);
@@ -76,19 +125,30 @@ PhaserIlluminated.prototype = {
         sprite.bmdIndex = 'illuminated-darkmask';
         sprite.bmd = bmd;
         sprite.refresh = function(){
-            this.darkMask.compute(this._game.width, this._game.height);
+            this.bmd.ctx.clearRect(0, 0, this.bmd.width, this.bmd.height);
+            this.darkMask.compute(this.bmd.width, this.bmd.height);
             this.darkMask.render(bmd.ctx);
             this.bmd.dirty = true;
         }
         sprite.getMask = function(){
             return this.darkMask;
         }
+        sprite.addLampSprite = function(lampSprite){
+            this.darkMask.lights.push(lampSprite.lamp);
+            this.refresh();
+        }
 
 
         return sprite;
     },
 
-    _createLighting: function(){
+    _createRectangleObject: function(x, y, w, h){
+        var obj = new illuminated.RectangleObject({topleft: new illuminated.Vec2(x, y),bottomright: new illuminated.Vec2(x+w, y+h)});
+        obj.originalX = x;
+        obj.originalY = y;
+        obj.originalW = w;
+        obj.originalH = h;
 
+        return obj;
     }
 }
